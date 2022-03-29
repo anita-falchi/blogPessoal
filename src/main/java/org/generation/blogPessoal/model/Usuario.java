5a7b55909b948872c88a99a4636f59e3f5f0009e
Anita <falchi.anita@gmail.com>
2022-03-29 16:28:37 -0300

    atualizando backend

--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -40,6 +40,7 @@ public class Usuario {
 	@Size (max = 5000, message = "O link da foto não pode ser maior do que 5000")
 	private String foto;
 	
+	private String tipo;
 	
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
@@ -104,6 +105,14 @@ public class Usuario {
 
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 	
 	
